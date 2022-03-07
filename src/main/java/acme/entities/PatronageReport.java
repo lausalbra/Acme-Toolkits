cd666d28edcca9dfbb56bc18ddeb605126e359ab
Alejandro Barba <alebarmor@alum.us.es>
2022-03-07 12:18:58 +0100

    Task 020: Completed
    Needed to be revised

--- src/main/java/acme/entities/PatronageReport.java
+++ src/main/java/acme/entities/PatronageReport.java
@@ -8,22 +8,19 @@ import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
-import acme.roles.Inventor;
-import acme.roles.Patron;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Patronage extends AbstractEntity{
+public class PatronageReport extends AbstractEntity{
 	
 	//Serialisation indentifier --------------------------------------------------------------- 
 	
@@ -31,35 +28,26 @@ public class Patronage extends AbstractEntity{
 
 	//Attributes --------------------------------------------------------------------------------
 	
-	@NotNull
-	protected Status status;
-	
-	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	@NotBlank
 	@Column(unique = true)
-	protected String code;
+	protected String sequenceNumber;
+	
+	@NotNull
+	@Past
+	protected Date creationMoment;
 	
 	@NotBlank
 	@Length(max = 256)
-	protected String legalStuff;
-	
-	@Positive
-	protected Integer budget;
-	
-	protected Date startPeriod;
-	
-	protected Date endPeriod;
+	protected String memorandum;
 	
 	@URL
 	protected String link;
 	
 	//Relationships -----------------------------------------------------------------------------
-	@NotNull
-	@Valid
-	@ManyToOne(optional = true)
-	Patron patron; 
 	
 	@NotNull
 	@Valid
 	@ManyToOne(optional = true)
-	Inventor inventor; 
+	Patronage patronage; 
+	
 }

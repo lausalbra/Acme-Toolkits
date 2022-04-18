62eb714fd188c848568a398cf6eb38ad749ec368
Alejandro Barba <alebarmor@alum.us.es>
2022-04-18 18:22:29 +0200

    # WARNING: head commit changed in the meantime
    Task 026-T1: Completed
    
    Se han realizado los test funcionales de los patrocinios

---
+++ src/test/java/acme/testing/patron/patronage/PatronPatronageShow.java
@@ -0,0 +1,35 @@
+package acme.testing.patron.patronage;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class PatronPatronageShow extends TestHarness {
+
+	@ParameterizedTest	
+	@CsvFileSource(resources = "/patron/patronage/show.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String status, final String code, final String legalStuff,
+		final String budget, final String fullName, final String email, final String startPeriod,
+		final String endPeriod, final String link) {
+		
+		super.signIn("patron1", "patron1");
+		super.clickOnMenu("Patron", "List my patronages");
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("status", status);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("legalStuff", legalStuff);
+		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("fullName", fullName);
+		super.checkInputBoxHasValue("email", email);
+		super.checkInputBoxHasValue("startPeriod", startPeriod);
+		super.checkInputBoxHasValue("endPeriod", endPeriod);
+		super.checkInputBoxHasValue("link", link);
+		
+		super.signOut();
+	}
+}

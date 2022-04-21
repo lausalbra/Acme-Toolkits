351f1e14b3a9dd986865f37903f3e39680bab8d0
fernando <fernandosilvaleon.us@gmail.com>
2022-04-21 11:14:11 +0200

    Task 027-T1: Completed
    Se han realizado los test funcionales de list user account
    (no se ordena bien con lo que puede fallar)

--- src/main/java/acme/features/any/account/AnyAccountListService.java
+++ src/main/java/acme/features/any/account/AnyAccountListService.java
@@ -25,6 +25,19 @@ public class AnyAccountListService implements AbstractListService<Any, UserAccou
 		return true;
 	}
 
+	
+//	final Comparator<UserAccount> c1=new Comparator<UserAccount>() {
+//		
+//		@Override
+//		public int compare(final UserAccount o1, final UserAccount o2) {
+//			if (o1.getId()<o2.getId()) {
+//				return 0;
+//			}else {
+//				return 1;
+//			}
+//		}
+//	};
+	
 	@Override
 	public Collection<UserAccount> findMany(final Request<UserAccount> request) {
 		assert request != null;
@@ -37,6 +50,9 @@ public class AnyAccountListService implements AbstractListService<Any, UserAccou
         final Set<UserAccount> res = new HashSet<UserAccount>();
         res.addAll(result);
         result.clear();
+//        final List<UserAccount> res2 = new ArrayList<UserAccount>();
+//        res2.addAll(res);
+//        Collections.sort(res2, this.c1);
         result.addAll(res);
 		return result;
 	}
@@ -46,7 +62,7 @@ public class AnyAccountListService implements AbstractListService<Any, UserAccou
 		assert request != null;
 		assert entity != null;
 		assert model != null;
-		
+
 		StringBuilder buffer;
         Collection<UserRole> roles;
 

da272851e63db41d52dd16354610b6ac2c84af74
alisanhos <alisanhos@alum.us.es>
2022-04-21 21:35:02 +0200

    Task 031: test all principal Item DONE

---
+++ src/test/java/acme/testing/anonymous/item/AnonymousItemListTest.java
@@ -0,0 +1,27 @@
+package acme.testing.anonymous.item;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnonymousItemListTest extends TestHarness{
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/item/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String name, final String itemType,
+								final String code, final String technology, final String retailPrice, final String fullname) {
+		
+		super.clickOnMenu("Anonymous", "List All Item");
+		
+		super.checkColumnHasValue(recordIndex, 0, name);
+		super.checkColumnHasValue(recordIndex, 1, itemType);
+		super.checkColumnHasValue(recordIndex, 2, code);
+		super.checkColumnHasValue(recordIndex, 3, technology);
+		super.checkColumnHasValue(recordIndex, 4, retailPrice);
+		super.checkColumnHasValue(recordIndex, 5, fullname);
+	}
+	
+}

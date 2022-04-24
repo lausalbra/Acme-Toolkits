9d604596f904b06597432ecdce394e1711b08f43
Alejandro Barba <alebarmor@alum.us.es>
2022-04-24 19:55:22 +0200

    Some fixes

--- src/main/java/acme/features/patron/patronDashboard/PatronPatronDashboardController.java
+++ src/main/java/acme/features/patron/patronDashboard/PatronPatronDashboardController.java
@@ -4,14 +4,12 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import acme.forms.PatronDashboard;
 import acme.framework.controllers.AbstractController;
 import acme.roles.Patron;
 
 @Controller
-@RequestMapping("/patron/patron-dashboard/")
 public class PatronPatronDashboardController extends AbstractController<Patron, PatronDashboard>{
 	  
 	  @Autowired
--- src/test/java/acme/testing/inventor/patronage/InventorPatronageListMine.java
+++ src/test/java/acme/testing/inventor/patronage/InventorPatronageListMine.java
@@ -16,6 +16,8 @@ public class InventorPatronageListMine extends TestHarness {
 		super.signIn("inventor1", "inventor1");
 		super.clickOnMenu("Inventor", "List my patronages");
 		
+		super.checkListingExists();
+		
 		super.checkColumnHasValue(recordIndex, 0, code);
 		super.checkColumnHasValue(recordIndex, 1, startPeriod);
 		super.checkColumnHasValue(recordIndex, 2, endPeriod);

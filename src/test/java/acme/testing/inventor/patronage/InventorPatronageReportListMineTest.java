00d45d0626e01e0cb9d360b47c39921cb380939f
Alejandro Barba <alebarmor@alum.us.es>
2022-05-16 17:24:19 +0200

    Task 052: Semifinished
    A la espera de añadir el módulo de spam

---
+++ src/test/java/acme/testing/inventor/patronage/InventorPatronageReportListMineTest.java
@@ -0,0 +1,25 @@
+package acme.testing.inventor.patronage;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class InventorPatronageReportListMineTest extends TestHarness {
+
+	@ParameterizedTest	
+	@CsvFileSource(resources = "/inventor/patronage-report/list-mine.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String sequenceNumber, final String creationMoment) {
+		
+		super.signIn("inventor1", "inventor1");
+		super.clickOnMenu("Inventor", "List my patronage reports");
+		
+		super.checkListingExists();
+		
+		super.checkColumnHasValue(recordIndex, 0, sequenceNumber);
+		super.checkColumnHasValue(recordIndex, 1, creationMoment);
+		super.signOut();
+	}
+}
